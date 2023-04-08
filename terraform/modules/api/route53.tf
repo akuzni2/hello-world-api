d1684ee5dfa0466cd72c5a97ce0a114a9e0f59d5
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-07 22:58:01 -0500

    github action build script

--- terraform/modules/api/route53.tf
+++ terraform/modules/api/route53.tf
@@ -1,29 +1,21 @@
-# TODO - complete once purchase a new domain name
+locals {
+  domain_name = "ak-ct-demo.link"
+}
 
-#locals {
-#  domain_name = "ak-ct-demo.link"
-#}
-#
-#data "aws_route53_zone" "my_zone" {
-#  name = local.domain_name
-#}
-#
-#module "acm" {
-#  source  = "terraform-aws-modules/acm/aws"
-#  version = "~> 3.0"
-#
-#  domain_name = local.domain_name
-#  zone_id     = data.aws_route53_zone.my_zone.id
-#}
-#
-#resource "aws_route53_record" "api" {
-#  zone_id = data.aws_route53_zone.my_zone.zone_id
-#  type    = "A"
-#
-#  alias {
-#    name                   = module.api_gateway.apigatewayv2_domain_name_configuration[0].target_domain_name
-#    zone_id                = module.api_gateway.apigatewayv2_domain_name_configuration[0].hosted_zone_id
-#    evaluate_target_health = false
-#  }
-#  name = "www.ak-ct-demo.link"
-#}
+data "aws_route53_zone" "my_zone" {
+  name = local.domain_name
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 3.0"
+
+  domain_name = local.domain_name
+  zone_id     = data.aws_route53_zone.my_zone.id
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.my_zone.id
+  name    = "www.${local.domain_name}"
+  type    = "A"
+}
