7e75e49f6d3d88668a167566f2e7b6c70da9dbc6
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-07 23:48:17 -0500

    fix mapping

--- terraform/modules/api/apgw.tf
+++ terraform/modules/api/apgw.tf
@@ -12,7 +12,7 @@ resource "aws_apigatewayv2_integration" "example_V2" {
 
 resource "aws_apigatewayv2_route" "example_v2" {
   api_id    = aws_apigatewayv2_api.hello_world_api_v2.id
-  route_key = "GET /v2/hello-world"
+  route_key = "GET /v1/hello-world"
   target    = "integrations/${aws_apigatewayv2_integration.example_V2.id}"
 }
 resource "aws_lambda_permission" "apigw_v2" {
@@ -26,7 +26,7 @@ resource "aws_lambda_permission" "apigw_v2" {
 
 resource "aws_apigatewayv2_stage" "hello_world_api_v2" {
   api_id      = aws_apigatewayv2_api.hello_world_api_v2.id
-  name        = "v1"
+  name        = "$default"
   auto_deploy = true
 }
 
