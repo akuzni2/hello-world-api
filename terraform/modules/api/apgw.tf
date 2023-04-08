6a0886f7b2d61cd4e27d68301c1fa37062c357d5
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-07 23:32:44 -0500

    test

--- terraform/modules/api/apgw.tf
+++ terraform/modules/api/apgw.tf
@@ -10,12 +10,12 @@ resource "aws_apigatewayv2_integration" "example_V2" {
   integration_uri = module.api_lambda.lambda_function_arn
 }
 
-resource "aws_apigatewayv2_route" "example" {
+resource "aws_apigatewayv2_route" "example_v2" {
   api_id    = aws_apigatewayv2_api.hello_world_api_v2.id
   route_key = "GET /v1/hello-world"
   target    = "integrations/${aws_apigatewayv2_integration.example_V2.id}"
 }
-resource "aws_lambda_permission" "apigw" {
+resource "aws_lambda_permission" "apigw_v2" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = module.api_lambda.lambda_function_arn
