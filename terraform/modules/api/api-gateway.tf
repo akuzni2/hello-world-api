ce8f75ee518d993b5a936804bdb7775af6c89a84
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-08 00:31:54 -0500

    delete old APIGW resource

--- terraform/modules/api/api-gateway.tf
+++ terraform/modules/api/api-gateway.tf
@@ -1,35 +1,32 @@
-resource "aws_apigatewayv2_api" "hello_world_api" {
-  name          = "ct-ak-api-hello-world"
+resource "aws_apigatewayv2_api" "hello_world_api_v2" {
+  name          = "ct-ak-api-hello-world-v2"
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_integration" "example" {
-  api_id           = aws_apigatewayv2_api.hello_world_api.id
+resource "aws_apigatewayv2_integration" "example_V2" {
+  api_id           = aws_apigatewayv2_api.hello_world_api_v2.id
   integration_type = "AWS_PROXY"
 
   integration_uri = module.api_lambda.lambda_function_arn
 }
 
-resource "aws_apigatewayv2_route" "example" {
-  api_id    = aws_apigatewayv2_api.hello_world_api.id
+resource "aws_apigatewayv2_route" "example_v2" {
+  api_id    = aws_apigatewayv2_api.hello_world_api_v2.id
   route_key = "GET /v1/hello-world"
-  target    = "integrations/${aws_apigatewayv2_integration.example.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.example_V2.id}"
 }
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
+resource "aws_lambda_permission" "apigw_v2" {
+  statement_id  = "AllowAPIGatewayInvokeV2"
   action        = "lambda:InvokeFunction"
   function_name = module.api_lambda.lambda_function_arn
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.hello_world_api.execution_arn}/*/*"
+  source_arn = "${aws_apigatewayv2_api.hello_world_api_v2.execution_arn}/*/*"
 }
 
-resource "aws_apigatewayv2_stage" "hello_world_api" {
-  api_id      = aws_apigatewayv2_api.hello_world_api.id
+resource "aws_apigatewayv2_stage" "hello_world_api_v2" {
+  api_id      = aws_apigatewayv2_api.hello_world_api_v2.id
   name        = "$default"
   auto_deploy = true
 }
 
-output "api_gateway_invoke_url" {
-  value = aws_apigatewayv2_api.hello_world_api.api_endpoint
-}
