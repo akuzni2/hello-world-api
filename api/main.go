8bf18d8a61137017f7676c842e8f4a20b43f03fa
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-08 00:38:53 -0500

    make api response JSON

--- api/main.go
+++ api/main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type ResponseBody struct {
+	Message string `json:"message"`
+}
+
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	responseBody := ResponseBody{
+		Message: "Hello World",
+	}
+
+	body, err := json.Marshal(responseBody)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Error marshalling JSON",
+		}, nil
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello World",
 		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(body),
 	}, nil
 }
 
