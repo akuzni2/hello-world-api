e8f432f68e17d1c305e8419d76d107053ebb74d8
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-08 00:02:25 -0500

    add back dns and v2 apigw

--- terraform/modules/api/apgw.tf
+++ terraform/modules/api/apgw.tf
@@ -1,32 +1,32 @@
-#resource "aws_apigatewayv2_api" "hello_world_api_v2" {
-#  name          = "ct-ak-api-hello-world-v2"
-#  protocol_type = "HTTP"
-#}
-#
-#resource "aws_apigatewayv2_integration" "example_V2" {
-#  api_id           = aws_apigatewayv2_api.hello_world_api_v2.id
-#  integration_type = "AWS_PROXY"
-#
-#  integration_uri = module.api_lambda.lambda_function_arn
-#}
-#
-#resource "aws_apigatewayv2_route" "example_v2" {
-#  api_id    = aws_apigatewayv2_api.hello_world_api_v2.id
-#  route_key = "GET /v1/hello-world"
-#  target    = "integrations/${aws_apigatewayv2_integration.example_V2.id}"
-#}
-#resource "aws_lambda_permission" "apigw_v2" {
-#  statement_id  = "AllowAPIGatewayInvokeV2"
-#  action        = "lambda:InvokeFunction"
-#  function_name = module.api_lambda.lambda_function_arn
-#  principal     = "apigateway.amazonaws.com"
-#
-#  source_arn = "${aws_apigatewayv2_api.hello_world_api_v2.execution_arn}/*/*"
-#}
-#
-#resource "aws_apigatewayv2_stage" "hello_world_api_v2" {
-#  api_id      = aws_apigatewayv2_api.hello_world_api_v2.id
-#  name        = "$default"
-#  auto_deploy = true
-#}
-#
+resource "aws_apigatewayv2_api" "hello_world_api_v2" {
+  name          = "ct-ak-api-hello-world-v2"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "example_V2" {
+  api_id           = aws_apigatewayv2_api.hello_world_api_v2.id
+  integration_type = "AWS_PROXY"
+
+  integration_uri = module.api_lambda.lambda_function_arn
+}
+
+resource "aws_apigatewayv2_route" "example_v2" {
+  api_id    = aws_apigatewayv2_api.hello_world_api_v2.id
+  route_key = "GET /v1/hello-world"
+  target    = "integrations/${aws_apigatewayv2_integration.example_V2.id}"
+}
+resource "aws_lambda_permission" "apigw_v2" {
+  statement_id  = "AllowAPIGatewayInvokeV2"
+  action        = "lambda:InvokeFunction"
+  function_name = module.api_lambda.lambda_function_arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.hello_world_api_v2.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "hello_world_api_v2" {
+  api_id      = aws_apigatewayv2_api.hello_world_api_v2.id
+  name        = "$default"
+  auto_deploy = true
+}
+
