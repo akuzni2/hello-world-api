068104ba1e5dc48964158606b41c5abed71700e2
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-07 20:22:47 -0500

    add lambda function

--- terraform/workspaces/dev/api/main.tf
+++ terraform/workspaces/dev/api/main.tf
@@ -2,7 +2,8 @@ module "cointracker_api" {
   source = "../../../modules/api"
 
   environment = var.environment
-  tags       = merge(var.tags)
-  aws_region = "us-east-1"
+  tags        = merge(var.tags)
+  aws_region  = "us-east-1"
 
+  initialize_lambda_empty = var.initialize_lambda_empty
 }
