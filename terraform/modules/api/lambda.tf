f3b031add6945218c67cdffb247d1efb88b1c4a1
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-07 20:45:45 -0500

    add lambda function

--- terraform/modules/api/lambda.tf
+++ terraform/modules/api/lambda.tf
@@ -2,22 +2,22 @@ locals {
   app_name = "ct-ak-api"
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "${local.app_name}-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+#resource "aws_iam_role" "lambda_role" {
+#  name = "${local.app_name}-lambda-role"
+#
+#  assume_role_policy = jsonencode({
+#    Version   = "2012-10-17"
+#    Statement = [
+#      {
+#        Action    = "sts:AssumeRole"
+#        Effect    = "Allow"
+#        Principal = {
+#          Service = "lambda.amazonaws.com"
+#        }
+#      }
+#    ]
+#  })
+#}
 
 
 module "api_lambda" {
@@ -31,7 +31,7 @@ module "api_lambda" {
   create_package         = false
   local_existing_package = "api-lambda.zip"
 
-  role_name               = aws_iam_role.lambda_role.name
+  role_name               = "ct-ak-api-lambda-role"
   ignore_source_code_hash = true
 }
 
@@ -41,30 +41,30 @@ resource "aws_cloudwatch_log_group" "example" {
   retention_in_days = 14
 }
 
-data "aws_iam_policy_document" "iam_policy_document" {
+#data "aws_iam_policy_document" "iam_policy_document" {
+#
+#  statement {
+#    effect  = "Allow"
+#    sid     = "CloudwatchLogsAccess"
+#    actions = [
+#      "logs:CreateLogGroup",
+#      "logs:CreateLogStream",
+#      "logs:PutLogEvents"
+#    ]
+#    resources = [
+#      "arn:aws:logs:*:*:*"
+#    ]
+#  }
+#
+#}
 
-  statement {
-    effect  = "Allow"
-    sid     = "CloudwatchLogsAccess"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = [
-      "arn:aws:logs:*:*:*"
-    ]
-  }
-
-}
-
-resource "aws_iam_policy" "lambda_policy" {
-  name   = "api_lambda_policy"
-  policy = data.aws_iam_policy_document.iam_policy_document.json
-}
-
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
-}
+#resource "aws_iam_policy" "lambda_policy" {
+#  name   = "api_lambda_policy"
+#  policy = data.aws_iam_policy_document.iam_policy_document.json
+#}
+#
+#
+#resource "aws_iam_role_policy_attachment" "lambda_logs" {
+#  role       = aws_iam_role.lambda_role.name
+#  policy_arn = aws_iam_policy.lambda_policy.arn
+#}
