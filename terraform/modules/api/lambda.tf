4b86c093a7795496412109f2b2a2ffde473e3a50
Alex Kuznicki <alexmkuznicki@gmail.com>
2023-04-07 20:34:36 -0500

    add lambda function

--- terraform/modules/api/lambda.tf
+++ terraform/modules/api/lambda.tf
@@ -22,27 +22,27 @@ resource "aws_iam_role" "lambda_role" {
 # only used to initialize
 data "archive_file" "dummy" {
   type        = "zip"
-  output_path = "${path.module}/api-lambda.zip"
+  output_path = "api-lambda.zip"
 }
 
-resource "aws_lambda_function" "api_lambda" {
+module "api_lambda" {
+  source = "terraform-aws-modules/lambda/aws"
+
   function_name = "ct_ak_hello_world_api"
   description   = "Says Hello World"
   handler       = "api-lambda-function"
   runtime       = "go1.x"
-  role          = aws_iam_role.lambda_role.arn
-  filename      = var.initialize_lambda_empty ? data.archive_file.dummy : null
 
-  lifecycle {
-    ignore_changes = [
-      "filename",
-    ]
-  }
+  create_package         = false
+  local_existing_package = "api-lambda.zip"
+
+  role_name               = aws_iam_role.lambda_role.name
+  ignore_source_code_hash = true
 }
 
 // log group
 resource "aws_cloudwatch_log_group" "example" {
-  name              = "/aws/lambda/${aws_lambda_function.api_lambda.function_name}"
+  name              = "/aws/lambda/${module.api_lambda.lambda_function_name}"
   retention_in_days = 14
 }
 
@@ -63,7 +63,7 @@ data "aws_iam_policy_document" "iam_policy_document" {
 
 }
 
-resource "aws_iam_policy" "reminder_lambda_policy" {
+resource "aws_iam_policy" "lambda_policy" {
   name   = "mr_reminder_lambda_policy"
   policy = data.aws_iam_policy_document.iam_policy_document.json
 }
@@ -71,5 +71,5 @@ resource "aws_iam_policy" "reminder_lambda_policy" {
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.reminder_lambda_policy.arn
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
